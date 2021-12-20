c7c2a64f648651e259f380ce60099a6811e6d8ce
e215731 <e215731@ie.u-ryukyu.ac.jp>
2021-12-21 03:07:12 +0900

    first commit

---
+++ app/src/main/java/Main.java
@@ -0,0 +1,8 @@
+import org.checkerframework.checker.units.qual.s;
+
+public class Main {
+    public static void main(String[] args){
+        String str = null;
+        System.out.println(str.length());
+    }
+}
